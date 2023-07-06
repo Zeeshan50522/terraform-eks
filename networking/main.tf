671103716b5a881c709957f210cfab7d8ba4e9fb
zeeshan50522 <zeeshanwaheed50522@gmail.com>
2023-07-06 19:28:31 +0200

    update terraform eks

--- networking/main.tf
+++ networking/main.tf
@@ -1,9 +1,40 @@
 resource "aws_vpc" "main" {
-    
+
   cidr_block       = var.CIDR
+  tags = {
+    Name = terraform.workspace
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count             = length(var.availability_zone)
+  vpc_id     = aws_vpc.main.id
+  availability_zone = element(var.availability_zone,count.index)
+  cidr_block = cidrsubnet(
+    var.CIDR,
+    ceil(log(length(var.availability_zone) * 2, 2)),
+    count.index
+  )
+}
+
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main.id
 
   tags = {
     Name = terraform.workspace
   }
 }
 
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = terraform.workspace
+  }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
 CIDR = "10.70.0.0/16"
 
-availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
+availability_zone = ["eu-central-1a", "eu-central-1b"]
 
